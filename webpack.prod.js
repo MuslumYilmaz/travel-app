4838c3ad12bb270d9c2b1e8ea20c5562888ba7e9
MuslumYilmaz <muslum@vizyon.ai>
2021-06-16 21:46:34 +0300

    image names

--- webpack.prod.js
+++ webpack.prod.js
@@ -29,6 +29,7 @@ module.exports = {
                   {
                     loader: 'file-loader',
                     options: {
+                        name: '[name].[ext]',
                         outputPath: 'icons/'
                     }
                   }
