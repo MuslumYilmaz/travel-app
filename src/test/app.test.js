4acd26ba4745c598e232e4336296710e138e4656
MuslumYilmaz <muslum@vizyon.ai>
2021-06-21 12:21:57 +0300

    test fix

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -2,6 +2,6 @@ import { calculateDay } from "../client/js/calculateDay";
 
 describe("Testing the date functionality", () => {
     test("Testing the oneDay() calculation", () => {
-        expect(calculateDay("2021-07-03")).toBe(16);
+        expect(calculateDay("2021-07-03")).toBe(11);
     });
 });
